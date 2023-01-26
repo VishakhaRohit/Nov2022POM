5420b057c869d8db81e8a5f19b40003c1167cf4e
VishakhaRohit <vishakhapokiya@gmail.com>
2023-01-25 20:34:06 -0500

    feature2

--- src/main/java/feature1/FeatureTwo.java
+++ src/main/java/feature1/FeatureTwo.java
@@ -2,9 +2,10 @@ package feature1;
 
 public class FeatureTwo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args)
+	{
+		System.out.println("feature2");
 	}
-
+	
+	
 }
