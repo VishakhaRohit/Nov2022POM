42a223c178a20842f2feba9dd5733c993ebca38c
VishakhaRohit <vishakhapokiya@gmail.com>
2023-01-25 20:20:27 -0500

    resolved conflict

--- src/main/java/com/qa/opencart/pages/DemoImp.java
+++ src/main/java/com/qa/opencart/pages/DemoImp.java
@@ -2,17 +2,9 @@ package com.qa.opencart.pages;
 
 public class DemoImp {
 
-<<<<<<< HEAD
 	public void order()
 	{
 		String name = "Naveen";
 	}
-	
-=======
-	public static void main(String[] args) {
 
-		String name = "Naveen";
-	}
-
->>>>>>> ed34cb5dee1bdcae7d2ef753e659cb44544ed231
 }
